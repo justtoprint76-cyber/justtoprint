fc4d94e2f774c9748b29438f5e83c8f27b463113
justtoprint76-cyber <justtoprint76@gmail.com>
2025-10-14 13:49:39 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>JUSTTOPRINT 🖤</h1>
+      <p>Il tuo e-commerce è online, in costruzione!</p>
+    </main>
+  );
+}
