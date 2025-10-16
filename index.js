7dbb03f4aa1e304f8222ffa0554dd2fc61bffee9
justtoprint76-cyber <justtoprint76@gmail.com>
2025-10-16 18:14:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-import dynamic from 'next/dynamic';
-
-// Home hero nera & oro
-const HomePage = dynamic(() => import('../components/HomePage'), { ssr: false });
-
-// Storefront (già creato): lo mostriamo sotto la hero per ora
-const Storefront = dynamic(() => import('../components/Storefront'), { ssr: false });
-
-export default function Home() {
-  return (
-    <>
-      <HomePage />
-      <div id="shop">
-        <Storefront />
-      </div>
-    </>
-  );
-}
+remove wrong index
 
