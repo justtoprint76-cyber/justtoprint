2c48266aef6cbe20ef34dbb65039d48e65aa0725
justtoprint76-cyber <justtoprint76@gmail.com>
2026-01-08 13:03:55 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* FAVICON */}
+        <link rel="icon" href="/favicon.png?v=2" />
+        <link rel="apple-touch-icon" href="/favicon.png?v=2" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
